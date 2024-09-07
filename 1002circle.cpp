01b9e1563cd44ec8028ce765c9a5926667444a46
leonidaspedro <leonidas@unochapeco.edu.br>
2024-09-06 21:33:58 -0300

    beecrowd

---
+++ 1002circle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	double a,r; 
+
+	cin >> r;
+	a = r * r * 3.14159;
+	cout << "A=" << setprecision(4)<< a <<endl;
+
+	return 0;
+
+}
+
